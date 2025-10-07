a8093ca7f4aa7ad0e5317480208e1064d6eae4ff
MyNameIsJuanMa <jhruby@uade.edu.ar>
2025-10-07 04:20:55 -0300

    TP 06

--- Grupo08_Unity/Assets/Trabajos Practicos/MenuPrincipal/MenuManager.cs
+++ Grupo08_Unity/Assets/Trabajos Practicos/MenuPrincipal/MenuManager.cs
@@ -6,41 +6,51 @@ public class MenuDeEscenas : MonoBehaviour
     // Cargar la escena del menú principal
     public void CargarMenu()
     {
-        SceneManager.LoadScene("MenuPrincipal/MenuDeEscenas");
+        SceneManager.LoadScene("MenuDeEscenas");
     }
 
     // TP01
     public void CargarEjercicio01()
     {
-        SceneManager.LoadScene("TP 01/Scenes/Ejercicio_01_SimpleList");
+        SceneManager.LoadScene("Ejercicio_01_SimpleList");
     }
 
     // TP02
     public void CargarEjercicio02()
     {
-        SceneManager.LoadScene("TP 02/Scenes/Ejercicio02_MyList");
+        SceneManager.LoadScene("Ejercicio02_MyList");
     }
 
     public void CargarEjercicio03()
     {
-        SceneManager.LoadScene("TP 02/Scenes/Ejercicio_03_Store");
+        SceneManager.LoadScene("Ejercicio_03_Store");
     }
 
     // TP03
     public void CargarEjercicio04()
     {
-        SceneManager.LoadScene("TP 03/Scenes/Ejercicio_04_Cola");
+        SceneManager.LoadScene("Ejercicio_04_Cola");
     }
 
     public void CargarEjercicio05()
     {
-        SceneManager.LoadScene("TP 03/Scenes/Ejercicio_05_Pila");
+        SceneManager.LoadScene("Ejercicio_05_Pila");
     }
 
     // TP05
     public void CargarEjercicio07()
     {
-        SceneManager.LoadScene("TP 05/Scenes/Ejercicio_07_Recursion");
+        SceneManager.LoadScene("Ejercicio_07_Recursion");
+    }
+
+    public void CargarEjercicio08()
+    {
+        SceneManager.LoadScene("Ejercicio_10_MyABBTree");
+    }
+
+    public void CargarEjercicio09()
+    {
+        SceneManager.LoadScene("Ejercicio_11_FuncionamientoABB");
     }
 
     // Salir del juego
